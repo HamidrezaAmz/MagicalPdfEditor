187798541e53cfeace0d9d1e5173dad7eaf7c4cd
Reza Amuzadeh <amuzadeh@vasl.ir>
2021-09-12 17:52:19 +0430

    increase version update annotation size

--- MagicalPEC/src/main/java/ir/vasl/magicalpec/utils/PublicValue.java
+++ MagicalPEC/src/main/java/ir/vasl/magicalpec/utils/PublicValue.java
@@ -4,7 +4,7 @@ public class PublicValue {
 
     public static final String KEY_SPECIAL_ID = "ITEXT__SPECIAL_ID";
 
-    public static final float DEFAULT_OCG_WIDTH = 50.0f;
-    public static final float DEFAULT_OCG_HEIGHT = 50.0f;
+    public static final float DEFAULT_OCG_WIDTH = 35.0f;
+    public static final float DEFAULT_OCG_HEIGHT = 35.0f;
 
 }
